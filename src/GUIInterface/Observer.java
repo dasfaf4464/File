c79f76afe1eb954732397e6c77d5349b90d4924b
dasfaf4464 <richchc@gmail.com>
2024-11-17 03:39:24 +0900

    project2 adjustment for project3

--- src/GUIInterface/Observer.java
+++ src/GUIInterface/Observer.java
@@ -1,8 +1,19 @@
 package GUIInterface;
 
-import java.io.File;
-
 public interface Observer {
-    abstract void newFileUpdate(File newFile);
-    abstract void fileSelectedUpdate(File file);
+    interface installerObserver {//설치버튼시 다이얼로그 프레젠터에서 프레임 프레젠터로 전달
+
+    }
+
+    interface fileCreatorObserver {//파일 생성시 메뉴 프레젠터에서 메인 패널 프레젠터(텍스트 뷰 업데이트), 사이드 패널 프레젠터로 전달(파일 트리 업데이트, 파일 리스트 업데이트)
+
+    }
+
+    interface fileDeleterObserver {//파일 삭제시 메뉴 프레젠터에서 메인 패널 프레젠터(텍스트 뷰 업데이트), 사이드 패널 프레젠터로 전달(파일 트리 업데이트, 파일 리스트 업데이트)
+
+    }
+
+    interface fileTreeSelectionObserver {//파일트리에서 선택시 메인 패널 프레젠터로 전달(텍스트에디터 업데이트)
+
+    }
 }
