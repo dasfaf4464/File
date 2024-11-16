d3395dfb5ef2a048c2922678d111a75a92c9084f
dasfaf4464 <richchc@gmail.com>
2024-11-17 03:30:53 +0900

    project2 adjustment for project3

--- src/guiPresenter/IDEPresenter.java
+++ src/guiPresenter/IDEPresenter.java
@@ -5,28 +5,25 @@ import guiModel.SettingManager;
 import guiView.Dialog;
 import guiView.MainFrame;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class IDEPresenter {
     private SettingManager settingManager;
     private ArrayList<Project> ideProjectList;
-    private final MainFrame mainFrame;
-    private final Dialog dialog;
 
     public IDEPresenter() {
         settingManager = new SettingManager();
         ideProjectList = new ArrayList<>();
-        mainFrame = new MainFrame();
-        dialog = new Dialog();
 
         initializeIDE();
     }
 
     private void initializeIDE() {
         if(settingManager.isInstalled()) {
-
+            SwingUtilities.invokeLater(MainFrame::new);
         } else {
-
+            SwingUtilities.invokeLater(Dialog::new);
         }
     }
 
